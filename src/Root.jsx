b8095c2ae561ba34bade6c6b90845d3e1d4e80e6
otto18 <otto@ceramic.global>
2024-02-26 17:43:50 -0500

    css updates

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,11 +3,11 @@ import Navbar from "./Navbar";
 
 export default function Root() {
   return (
-    <>
+    <div className="app_container">
       <Navbar />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
